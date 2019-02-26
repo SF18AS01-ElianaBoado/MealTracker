3473d8953f9b2815e64cb739ff23734cb27a08a9
Eliana Boado <45747984+ElianaBoado@users.noreply.github.com>
2019-02-26 11:56:49 -0500

    Initial commit

---
+++ MealTracker/Food.swift
@@ -0,0 +1,14 @@
+//
+//  Food.swift
+//  MealTracker
+//
+//  Created by Eliana Boado on 2/26/19.
+//  Copyright © 2019 Eliana Boado. All rights reserved.
+//
+
+import Foundation
+
+struct Food {
+    var name: String;
+    var description: String;
+}
---
+++ MealTracker/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  MealTracker
+//
+//  Created by Eliana Boado on 2/26/19.
+//  Copyright © 2019 Eliana Boado. All rights reserved.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String;
+    var food: [Food];
+}
